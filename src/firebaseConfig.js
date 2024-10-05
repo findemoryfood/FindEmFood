677a96551042effbf6c1a32686cb8c35c5a7e8fe
davidc3069 <111481596+davidc3069@users.noreply.github.com>
2024-10-05 16:40:15 -0400

    added default firebaseConfig file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVDxq4BnlyNupqAvxbb0VnCBrPjHX1E2A",
+  authDomain: "find-em-food.firebaseapp.com",
+  databaseURL: "https://find-em-food-default-rtdb.firebaseio.com",
+  projectId: "find-em-food",
+  storageBucket: "find-em-food.appspot.com",
+  messagingSenderId: "593345581471",
+  appId: "1:593345581471:web:aa587e53255f784e64edaa",
+  measurementId: "G-0S1P4VR4SN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
