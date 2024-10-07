15553c32c5b7fb4987166e31c9b027fa6153db15
Ja'Zmin McKeel <72769753+JaZminMcKeel@users.noreply.github.com>
2024-10-06 20:02:41 -0400

    Update GPS.js

--- src/GPS.js
+++ src/GPS.js
@@ -118,7 +118,7 @@ const GPS = () => {
     // Load the Google Maps API script
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDex9CSyi3X6ig_8iBL1mBdsmJIKMgrGiA&callback=initMap`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=API_Key=initMap`;
         script.async = true;
         script.defer = true;
         document.body.appendChild(script);
