84e084c2078045cc7aa877e50487c365faa074e3
Katie Park <147112323+KJP952@users.noreply.github.com>
2024-11-11 13:03:00 -0500

    Created Indoor Map

---
+++ src/floorplans/IndoorMap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const IndoorMap = () => {
+  return (
+    <div>
+      <h1>Welcome to the Map</h1>
+      <iframe href="https://www.mappedin.com/" 
+      title="Mappedin Map" 
+      name="Mappedin Map" 
+      allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com" 
+      scrolling="no" 
+      width="100%" 
+      height="650" 
+      frameborder="0" 
+      style="border:0" src="https://app.mappedin.com/map/6732310c66ce60000b9169e8?embedded=true"></iframe>
+    </div>
+  );
+};
+
+export default IndoorMap;
