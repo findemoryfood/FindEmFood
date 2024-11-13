12ece4a5786ba243612729ee988620a76d7a43ed
jazminmckeel <jmckeel@emory.edu>
2024-11-13 18:53:54 -0500

    -  NavBar Fixed - Side Bar Added   - When the screen in minimized or if the product is used on a small screen the main navbar is hidden and the side bar appears - Page Content overlap fixed

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,47 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './assets/logo.png';
-import './styles/NavBar.css'; // Link to the CSS file for styles
+import './styles/NavBar.css';
 
 const NavBar = ({ isLoggedIn, user }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  // For the sidebar implementation for small devices and minimized screens
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="navbar">
-      {/* Left Links */}
-      <nav className="nav-links nav-left">
+      {/* Sidebar Toggle Button for small screens */}
+      <button className="sidebar-toggle" onClick={toggleSidebar}>☰</button>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${isSidebarOpen ? 'show' : ''}`}>
         <ul>
           <li>
-            <Link to="/GPS">GPS</Link>
+            <Link to="/GPS" onClick={toggleSidebar}>GPS</Link>
           </li>
           <li>
-            <Link to="/FoodList">Food List</Link>
+            <Link to="/FoodList" onClick={toggleSidebar}>Food List</Link>
+          </li>
+          <li>
+            <Link to="/Setting" onClick={toggleSidebar}>Settings</Link>
+          </li>
+          <li>
+            {isLoggedIn ? (
+              <Link to="/UserProfile" onClick={toggleSidebar}>
+                <span>{user?.username}</span>
+              </Link>
+            ) : (
+              <Link to="/OrgSignIn" onClick={toggleSidebar}>Log In</Link>
+            )}
           </li>
         </ul>
-      </nav>
-
-      {/* Logo */}
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
       </div>
 
-      {/* Right Links */}
-      <nav className="nav-links nav-right">
+      {/* Main navbar for larger screens of full screen on computer */}
+      <div className="main-nav">
+        {/* Left Links */}
+        <nav className="nav-links nav-left">
           <ul>
-              <li>
-                  <Link to="/Setting">Settings</Link>
-              </li>
-              <li>
-                  {isLoggedIn ? (
-                      <Link to="/UserProfile">
-                          <span>{user?.username}</span>
-                      </Link>
-                  ) : (
-                      <Link to="/OrgSignIn">Log In</Link>
-                  )}
-              </li>
+            <li>
+              <Link to="/GPS">GPS</Link>
+            </li>
+            <li>
+              <Link to="/FoodList">Food List</Link>
+            </li>
           </ul>
-      </nav>
+        </nav>
+
+        {/* Logo with Product Name */}
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </Link>
+          {/*<div className="logo-text">Find 'Em Food</div>*/}
+        </div>
+
+        {/* Right Links */}
+        <nav className="nav-links nav-right">
+          <ul>
+            <li>
+              <Link to="/Setting">Settings</Link>
+            </li>
+            <li>
+              {isLoggedIn ? (
+                <Link to="/UserProfile">
+                  <span>{user?.username}</span>
+                </Link>
+              ) : (
+                <Link to="/OrgSignIn">Log In</Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
